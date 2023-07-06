09e0dbbad06d950881b35e7f387aa1357b80d55d
Titanul1 <57775360+Titanul1@users.noreply.github.com>
2023-07-06 12:46:36 +0300

    Update SimpleFakeNews2.py

--- SimpleFakeNews2.py
+++ SimpleFakeNews2.py
@@ -1,37 +1,41 @@
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 import subprocess
+from datasets import load_dataset
 
-subprocess.run(["pip", "install", "scikit-learn"])
-
-# Import the required module
-from sklearn.model_selection import train_test_split
-
-# Install packages using pip
+# Install required packages
 subprocess.run(["pip", "install", "huggingface"])
 subprocess.run(["pip", "install", "huggingface_hub"])
 subprocess.run(["pip", "install", "transformers"])
 subprocess.run(["pip", "install", "datasets"])
 
-# BIP Project 6 - Karell, Antonio, Hanna
-# adapted from from https://www.youtube.com/watch?v=ZE2DANLfBIs&ab_channel=NeuralNine
 # Load dataset
-from datasets import load_dataset
 data = load_dataset("liar")
 
 # Simplify the labeling to binary
-labelarray = []
+label_array = []
 for i in range(len(data["test"]["label"])):
     if data["test"]["label"][i] == 3:
-        labelarray.append(0)
+        label_array.append(0)
     else:
-        labelarray.append(1)
+        label_array.append(1)
 
-X, y = data['test']['statement'], labelarray
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+X, y = data['test']['statement'], label_array
+
+# Shuffle the indices of your data
+indices = np.random.permutation(len(X))
+
+# Define the ratio for the train-test split
+train_ratio = 0.8  # 80% for training, 20% for testing
+
+# Compute the split index
+split_index = int(len(X) * train_ratio)
+
+# Split the data and labels into training and testing sets
+X_train, X_test = X[indices[:split_index]], X[indices[split_index:]]
+y_train, y_test = y[indices[:split_index]], y[indices[split_index:]]
 
 # Create TfidfVectorizer
 vectorizer = TfidfVectorizer(stop_words="english")
@@ -51,17 +55,3 @@ text = "Some new text to predict"
 vectorized_text = vectorizer.transform([text])
 prediction = classifier.predict(vectorized_text)
 print("Prediction:", prediction)
-
-
-# Shuffle the indices of your data
-indices = np.random.permutation(len(X))
-
-# Define the ratio for the train-test split
-train_ratio = 0.8  # 80% for training, 20% for testing
-
-# Compute the split index
-split_index = int(len(X) * train_ratio)
-
-# Split the data and labels into training and testing sets
-X_train, X_test = X[indices[:split_index]], X[indices[split_index:]]
-y_train, y_test = y[indices[:split_index]], y[indices[split_index:]]
