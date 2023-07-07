1247e7ae19c4651b846c06b7473c0ce3adcd4b30
Titanul1 <57775360+Titanul1@users.noreply.github.com>
2023-07-07 12:13:56 +0300

    Update SimpleFakeNews2.py

--- SimpleFakeNews2.py
+++ SimpleFakeNews2.py
@@ -8,11 +8,14 @@ import requests
 import csv
 import streamlit as st
 import logging
+import mymodel as m
 from bs4 import BeautifulSoup
 
 st.write("""
 #Ceva pe acolo Boss
 Hopa *CF GEIGELE?*""")
+
+st.write(m.run(window=10))
 
 # Define constants
 FAKE_REAL_NEWS_CSV_URL = 'https://drive.google.com/uc?id=14HFSVmD84uQai5IXDGBGHHvJ9SbEdPsA'
