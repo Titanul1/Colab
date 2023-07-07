11cfd3593f14f6d189168ca53eda1c87003c8f45
Titanul1 <57775360+Titanul1@users.noreply.github.com>
2023-07-07 15:14:45 +0300

    Update SimpleFakeNews2.py

--- SimpleFakeNews2.py
+++ SimpleFakeNews2.py
@@ -12,9 +12,9 @@ import logging
 from bs4 import BeautifulSoup
 
 st.title('FakeNews')
+st.text_input('Type in the URL of the webpage:','')
 
-
-ceva = st.text_area(st.text_input('Type in the URL of the webpage:',''))
+ceva = st.text_area('','')
 
 
 # Define constants
